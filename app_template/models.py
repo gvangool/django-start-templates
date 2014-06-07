5c8e1d9507ae6ad3233773a7e0677812972f1d44
Gert Van Gool <gertvangool@gmail.com>
2014-06-07 14:16:57 +0200

    Adds Python3 compatibility in models

--- app_template/models.py
+++ app_template/models.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 # Docs @ https://docs.djangoproject.com/en/dev/ref/models/
+
+#@python_2_unicode_compatible
+#class MyModel(models.Model):
+#    def __str__(self):
+#        return _('MyModel: id %(pk)d') % {'pk': self.pk}
